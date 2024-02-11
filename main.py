75f06d79fd2f3bf966125fdca5d16595cbbe46dc
Rafal Wisniewski <2krafal.wisniewski@gmail.com>
2024-02-11 15:08:00 +0100

    add handling all files in directory

--- main.py
+++ main.py
@@ -7,23 +7,30 @@ import logging
 import sys
 import os
 import shutil
+from pathlib import Path
 
 log = logging.getLogger(__name__)
 name_dict = {}
-parser = argparse.ArgumentParser(prog='RenameEPUB', description='program change name of file using metadata from epub')
+parser = argparse.ArgumentParser(prog="RenameEPUB", description="program change name of file using metadata from epub")
 
-parser.add_argument('-f', '--file')
-parser.add_argument('-F', '--format', default="{creator} - {title}", help="format of name, default is '{creator} - {title}'")
-parser.add_argument('-d', '--directory', help="default is '.'")
-parser.add_argument('-v', '--verbose', action="store_true", help="adds extra verbose")
-parser.add_argument('-l', '--logging', choices=['DEBUG', 'INFO', 'WARNING', "ERROR", "CRITICAL"], default='DEBUG', help="logging level")
-parser.add_argument('-r', '--replace', action="store_true", help="should replace file instead of creating copy")
-parser.add_argument('-D', '--destination', default=".", help="destination folder to copy file with new name")
+parser.add_argument("-f", "--file")
+parser.add_argument(
+    "-F", "--format", default="{creator} - {title}", help="format of name, default is '{creator} - {title}'"
+)
+parser.add_argument("-d", "--directory", help="default is '.'")
+parser.add_argument("-v", "--verbose", action="store_true", help="adds extra verbose")
+parser.add_argument(
+    "-l", "--logging", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], default="INFO", help="logging level"
+)
+parser.add_argument("-r", "--replace", action="store_true", help="should replace file instead of creating copy")
+parser.add_argument("-o", "--output", help="output folder to copy file with new name")
 args = parser.parse_args()
 
-def exit_and_log(exit_code:int):
+
+def exit_and_log(exit_code: int):
     log.debug(f"exit with code: {exit_code}")
     sys.exit(exit_code)
+
 
 def validate_args():
     if args.file and args.directory:
@@ -31,7 +38,11 @@ def validate_args():
         exit_and_log(1)
     if not args.file and not args.directory:
         log.debug("setting directory to '.'")
-        args.directory = '.'
+        args.directory = "."
+    if args.replace and args.output:
+        log.error("you can not set output and replace at the same time")
+        exit_and_log(1)
+
 
 def setup_logger():
     numeric_level = getattr(logging, args.logging.upper(), None)
@@ -40,36 +51,38 @@ def setup_logger():
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(formatter)
     log.addHandler(handler)
-    
+
 
 def get_metadata(book: EpubBook, name: str) -> str:
     log.debug(f"getting metadata: '{name}'")
-    metadata = book.get_metadata('DC', name)
+    metadata = book.get_metadata("DC", name)
     if not metadata:
         log.warning(f"there is no metadata named: {name}")
         return ""
     return metadata[0][0]
-    
+
 
 def get_names_from_format() -> list[str]:
     return set(re.findall(r"{([^}]+)}", args.format))
 
 
 def get_new_filename(old_filename: str) -> str:
-    dirname = os.path.dirname(old_filename)
-    log.debug(f"processing file: '{old_filename}'")
-    new_name = str(args.format)
-    book = epub.read_epub(old_filename, {"ignore_ncx": True})
-    for name in get_names_from_format():
-        metadata = get_metadata(book, name)
-        new_name = new_name.replace(f"{{{name}}}", metadata)
-    new_name = f"{new_name}.epub"
-    log.debug(f"new name: '{new_name}'")
-    new_filename =  os.path.join(dirname, new_name)
+    formatted_name = reformat_name(old_filename)
+    if args.output:
+        dirname = args.output
+        Path(dirname).mkdir(parents=True, exist_ok=True)
+    else:
+        dirname = os.path.dirname(old_filename)
+    new_filename = os.path.join(dirname, formatted_name)
+    new_filename = add_suffix_if_needed(new_filename)
+    log.info(f"new filename: '{new_filename}'")
+    return new_filename
+
+def add_suffix_if_needed(new_filename):
     while os.path.isfile(new_filename):
         log.debug(f"file {new_filename} already exists, adding suffix...")
         file_name, file_extension = os.path.splitext(new_filename)
-        match = re.search(r'\((\d)\)$', file_name)
+        match = re.search(r"\((\d)\)$", file_name)
         if match:
             index = int(match.group(1))
             file_name = file_name.removesuffix(f" ({index})")
@@ -78,15 +91,24 @@ def get_new_filename(old_filename: str) -> str:
         else:
             new_filename = f"{file_name} (0){file_extension}"
             log.debug(f"new suffix:' (0)'")
-    log.info(f"new filename: '{new_name}'")
     return new_filename
 
+def reformat_name(old_filename):
+    log.debug(f"processing file: '{old_filename}'")
+    formatted_name = str(args.format)
+    book = epub.read_epub(old_filename, {"ignore_ncx": True})
+    for name in get_names_from_format():
+        metadata = get_metadata(book, name)
+        formatted_name = formatted_name.replace(f"{{{name}}}", metadata)
+    formatted_name = f"{formatted_name}.epub"
+    log.debug(f"new name: '{formatted_name}'")
+    return formatted_name
 
 
 def handle_change_file(old_filename: str) -> int:
     if not str(old_filename).endswith(".epub"):
-            log.error("file extension has to be '.epub'!")
-            return 1
+        log.error("file extension has to be '.epub'!")
+        return 1
     try:
         new_filename = get_new_filename(old_filename)
         if args.replace:
@@ -108,7 +130,12 @@ def main():
     if old_filename:
         exit_code = handle_change_file(old_filename)
         exit_and_log(exit_code)
-        
+    directory = args.directory
+    for file in os.listdir(directory):
+        file_and_path = os.path.join(directory, file)
+        if os.path.isfile(file_and_path) and file_and_path.endswith(".epub"):
+            handle_change_file(file_and_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
